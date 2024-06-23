020d212f828784549e29155fa687052cb80d90a3
White Torn <FaustusTorn@protonmail.com>
2024-06-23 12:40:34 +0400

    Initialised

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView
+        .OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView
+                .setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    firstFragment firstFragment = new firstFragment();
+    secondFragment secondFragment = new secondFragment();
+    thirdFragment thirdFragment = new thirdFragment();
+    forthFragment forthFragment = new forthFragment();
+    fifthFragment fifthFragment = new fifthFragment();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentOne, firstFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentTwo, secondFragment).commit();
+            return true;
+        } else if (item.getItemId() == R.id.person) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentOne, thirdFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentTwo, secondFragment).commit();
+            return true;
+        }  else if (item.getItemId() == R.id.portfolio) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentOne, firstFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentTwo, secondFragment).commit();
+            return true;
+        } else if (item.getItemId() == R.id.gallery) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentOne, firstFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentTwo, secondFragment).commit();
+            return true;
+        }
+        else if (item.getItemId() == R.id.settings) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentOne, forthFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentTwo, thirdFragment).commit();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/project/fifthFragment.java
@@ -0,0 +1,32 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link fifthFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class fifthFragment extends Fragment {
+
+
+
+    public fifthFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fifth, container, false);
+    }
+}
